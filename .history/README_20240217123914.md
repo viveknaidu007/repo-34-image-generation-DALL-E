035ecedecb975d01f7911765b26c66dcd09fe584
viveknaidu007 <vivek.pavankalyan@gmail.com>
2024-02-17 13:24:46 +0530

    updated

---
+++ .history/README_20240217123914.md
@@ -0,0 +1,2 @@
+# repo-34-image-generation-DALL-E
+we r creating a web app for genrating image using api key
---
+++ .history/dalle - image generation - web app_20240217124130.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+@author: vivek
+"""
+
+
+import openai
+import urllib.request
+from PIL import Image
+import streamlit as st
+
+
+
+openai.api_key = "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"
+
+def generate_image(image_description):
+
+  img_response = openai.Image.create(
+    prompt = image_description,
+    n=1,
+    size="512x512")
+  
+
+  img_url = img_response['data'][0]['url']
+
+  urllib.request.urlretrieve(img_url, 'img.png')
+
+  img = Image.open("img.png")
+  
+  return img
+
+
+
+# page title
+st.title('DALL.E - Image Generation - OpenAI')
+
+# text input box for image recognition
+img_description = st.text_input('Image Desription')
+
+if st.button('Generate Image'):
+    
+    generated_img = generate_image(img_description)
+    st.image(generated_img)
+    
